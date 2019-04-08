0bfb6c98aa41bd5cd82e80d19a1127ce8756391b
Henrik Poulsen <henrik_v_poulsen@hotmail.com>
2019-04-08 09:28:23 +0200

    Add project files.

---
+++ ClassDiagram/Migrations/20190402065515_EntityDB.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClassDiagram.Migrations
+{
+    public partial class EntityDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<DateTime>(nullable: false),
+                    Streetname = table.Column<string>(nullable: true),
+                    HouseNumber = table.Column<string>(nullable: true),
+                    Floor = table.Column<string>(nullable: true),
+                    Door = table.Column<string>(nullable: true),
+                    Zipcode = table.Column<int>(nullable: false),
+                    City = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Laundry",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Pin = table.Column<int>(nullable: false),
+                    ShowID = table.Column<int>(nullable: false),
+                    OpeningTime = table.Column<DateTime>(nullable: false),
+                    ClosingTime = table.Column<DateTime>(nullable: false),
+                    ScheduleLimit = table.Column<int>(nullable: false),
+                    DefaultDuration = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Laundry", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Machine",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Status = table.Column<int>(nullable: false),
+                    MachineDuration = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Machine", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Email = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    LastLogin = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Email);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AdminUser",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<DateTime>(nullable: false),
+                    PersonDataEmail = table.Column<string>(nullable: true),
+                    CreateDeleteMachine = table.Column<bool>(nullable: false),
+                    ChangeDuration = table.Column<bool>(nullable: false),
+                    ChangeOpeningHours = table.Column<bool>(nullable: false),
+                    DeleteEndUsers = table.Column<bool>(nullable: false),
+                    DeleteAdmins = table.Column<bool>(nullable: false),
+                    ChangeShowID = table.Column<bool>(nullable: false),
+                    ChangeScheduleLimit = table.Column<bool>(nullable: false),
+                    IsMaster = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminUser", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_AdminUser_Person_PersonDataEmail",
+                        column: x => x.PersonDataEmail,
+                        principalTable: "Person",
+                        principalColumn: "Email",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EndUser",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<DateTime>(nullable: false),
+                    PersonDataEmail = table.Column<string>(nullable: true),
+                    AddressID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EndUser", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_EndUser_Address_AddressID",
+                        column: x => x.AddressID,
+                        principalTable: "Address",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_EndUser_Person_PersonDataEmail",
+                        column: x => x.PersonDataEmail,
+                        principalTable: "Person",
+                        principalColumn: "Email",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<DateTime>(nullable: false),
+                    Time = table.Column<DateTime>(nullable: false),
+                    EndUserID = table.Column<int>(nullable: true),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Schedules_EndUser_EndUserID",
+                        column: x => x.EndUserID,
+                        principalTable: "EndUser",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdminUser_PersonDataEmail",
+                table: "AdminUser",
+                column: "PersonDataEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EndUser_AddressID",
+                table: "EndUser",
+                column: "AddressID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EndUser_PersonDataEmail",
+                table: "EndUser",
+                column: "PersonDataEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_EndUserID",
+                table: "Schedules",
+                column: "EndUserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdminUser");
+
+            migrationBuilder.DropTable(
+                name: "Laundry");
+
+            migrationBuilder.DropTable(
+                name: "Machine");
+
+            migrationBuilder.DropTable(
+                name: "Schedules");
+
+            migrationBuilder.DropTable(
+                name: "EndUser");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
---
+++ ClassDiagram/Models/Models.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClassDiagram.Models
+{
+    public class EntityDB : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = 192.168.4.224; Initial Catalog = TestDB2; Persist Security Info = True; User ID = hepo; Password = ***********");
+        }
+        //public DbSet<DBDefaults> DBDefaults { get; set; }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<EndUser> EndUser { get; set; }
+        public DbSet<AdminUser> AdminUser { get; set; }
+        public DbSet<Laundry> Laundry { get; set; }
+        public DbSet<Machine> Machine { get; set; }
+        public DbSet<Schedules> Schedules { get; set; }
+    }
+
+    public class DBDefaults
+    {
+        public int ID { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Deleted { get; set; }
+    }
+
+    public class Person
+    {
+        [Key]
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public DateTime LastLogin { get; set; }
+    }
+
+    public class EndUser : DBDefaults
+    {
+        public Person PersonData { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class AdminUser : DBDefaults
+    {
+        public Person PersonData { get; set; }
+        public bool CreateDeleteMachine { get; set; }
+        public bool ChangeDuration { get; set; }
+        public bool ChangeOpeningHours { get; set; }
+        public bool DeleteEndUsers { get; set; }
+        public bool DeleteAdmins { get; set; }
+        public bool ChangeShowID { get; set; }
+        public bool ChangeScheduleLimit { get; set; }
+        public bool IsMaster { get; set; }
+        //En adminUser kan have flere vaskerier, men et vaskeri kan godt have flere adminUsers
+        List<Laundry> Laundry = new List<Laundry>(); 
+    }
+
+    public enum ShowIDEnum { Anonym, Adresse, Navn }
+
+    public class Laundry : DBDefaults
+    {
+        public string Name { get; set; }
+        public int Pin { get; set; }
+        public ShowIDEnum ShowID { get; set; }
+        public DateTime OpeningTime { get; set; }
+        public DateTime ClosingTime { get; set; }
+        public int ScheduleLimit { get; set; }
+        public int DefaultDuration { get; set; }
+        List<Address> Addresses = new List<Address>();
+        List<Machine> Machines = new List<Machine>();
+    }
+
+    public enum MachineStatusEnum { OK, OutOfOrder, TechnicianCalled }
+
+    public class Machine : DBDefaults
+    {
+        public string Name { get; set; }
+        public string  Description { get; set; }
+        public MachineStatusEnum Status { get; set; }
+        public int? MachineDuration { get; set; }
+        List<Schedules> ScheduleList = new List<Schedules>();
+    }
+
+    public enum ScheduleStatusEnum { Free, Booked, OutOfOrder }
+
+    public class Schedules : DBDefaults
+    {
+        public DateTime Time { get; set; }
+        public EndUser EndUser { get; set; }
+        public ScheduleStatusEnum Status { get; set; }
+    }
+
+    public class Address : DBDefaults
+    {
+        public string Streetname { get; set; }
+        public string HouseNumber { get; set; }
+        public string Floor { get; set; }
+        public string Door { get; set; }
+        public int Zipcode { get; set; }
+        public string City { get; set; }
+    }
+}
+
+
